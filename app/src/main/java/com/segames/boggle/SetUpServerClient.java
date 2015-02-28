3fcbe27069e984bdfcd6218cd239b49fb37ce879
edwarda <edwarda@pdx.edu>
2015-02-28 11:00:02 -0800

    Added onResume chatservice check. Should keep connection after closing this screen with a pairing selection of the same device as earlier

--- app/src/main/java/com/segames/boggle/SetUpServerClient.java
+++ app/src/main/java/com/segames/boggle/SetUpServerClient.java
@@ -151,6 +151,22 @@ public class SetUpServerClient extends ActionBarActivity implements View.OnClick
         }
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mChatService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+                // Start the Bluetooth chat services
+                mChatService.start();
+            }
+        }
+    }
+
     private final Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
